f9591eb48d242cb26e033be29177539e16d47ef4
LaiZhiQun <wcjh0911@gmail.com>
2023-03-15 17:23:16 +0800

    新增: 找不到對應網址的頁面

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,10 @@ const routes = [
         component: () => import('../views/front/FormView.vue')
       }
     ]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container text-white d-flex flex-column align-items-center justify-content-center">
+    <h1 class="mt-10">404</h1>
+    <p class="mt-10">找不到頁面，請輸入正確網址</p>
+  </div>
+</template>
